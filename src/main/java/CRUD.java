ff38f1469e31a1793848c46d257a0079fe48ee8c
Luiz Felipe <lfpereira323@gmail.com>
2023-11-28 15:48:12 -0300

    primeiro commit

---
+++ src/main/java/CRUD.java
@@ -0,0 +1,221 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.Scanner;
+
+public class CRUD {
+    EntityManagerFactory emf;
+  public CRUD(){
+      emf = Persistence.createEntityManagerFactory("ORMPRJ");
+  }
+  public void endSession(){
+      emf.close();
+  }
+  public void addMidia(int ID, String nome, Genero genero, int anoLancamento, Diretor diretor){
+      EntityManager em = emf.createEntityManager();
+      Midia m1 =  new Midia(ID, nome, genero, anoLancamento, diretor);
+      em.getTransaction().begin();
+      em.persist(m1);
+      em.getTransaction().commit();
+      em.close();
+  }
+
+  public Midia searchMidia(int ID){
+      EntityManager em = emf.createEntityManager();
+      em.getTransaction().begin();
+      Midia m1 = em.find(Midia.class,ID);
+      em.close();
+      return m1;
+
+  }
+
+  public void updateMidia(int ID){
+      Scanner scan = new Scanner(System.in);
+      Midia m1 = searchMidia(ID);
+      System.out.println("Digite 1 para atualizar o nome ou 2 para o ano de lançamento");
+      int resposta = scan.nextInt();
+      if(resposta==1){
+          System.out.println("Digite o novo nome: ");
+          String novoNome = scan.next();
+          scan.nextLine();
+          m1.setNome(novoNome);
+      } else if (resposta==2) {
+          System.out.println("Digite o ano de lançamento: ");
+          int novoAno = scan.nextInt();
+          m1.setAnoLancamento(novoAno);
+
+      }
+      EntityManager em = emf.createEntityManager();
+      em.getTransaction().begin();
+      em.merge(m1);
+      em.getTransaction().commit();
+      em.close();
+  }
+  public void deleteMidia(int ID){
+      Scanner scan = new Scanner(System.in);
+      EntityManager em = emf.createEntityManager();
+
+      try {
+          Midia m1 = em.find(Midia.class, ID);
+
+          if (m1 != null) {
+              System.out.println("Confirmar exclusão? [y/n]");
+              String resposta = scan.nextLine();
+
+              if (resposta.equals("y")) {
+                  em.getTransaction().begin();
+                  em.remove(m1);
+                  em.getTransaction().commit();
+                  System.out.println("Mídia excluída com sucesso");
+              } else if (resposta.equals("n")) {
+                  System.out.println("Operação cancelada");
+              } else {
+                  System.out.println("Resposta inválida");
+              }
+          } else {
+              System.out.println("Mídia não encontrada");
+          }
+      } catch (Exception e) {
+          System.out.println("Ocorreu um erro durante a exclusão: " + e.getMessage());
+          if (em.getTransaction().isActive()) {
+              em.getTransaction().rollback();
+          }
+      } finally {
+          em.close();
+          emf.close();
+      }
+
+  }
+  public void addGenero(int ID, String nome){
+      EntityManager em = emf.createEntityManager();
+      Genero g1 = new Genero(ID, nome);
+      em.getTransaction().begin();
+      em.persist(g1);
+      em.getTransaction().commit();
+      em.close();
+
+  }
+  public Genero searchGenero(int ID){
+      EntityManager em = emf.createEntityManager();
+      Genero genero = em.find(Genero.class,ID);
+      em.close();
+      return genero;
+
+  }
+
+    public void updateGenero(int ID){
+        Scanner scan = new Scanner(System.in);
+        Genero genero = searchGenero(ID);
+
+        System.out.println("Digite o novo nome do gênero: ");
+        String novoNome = scan.nextLine();
+        genero.setNome(novoNome);
+
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.merge(genero);
+        em.getTransaction().commit();
+        em.close();
+        System.out.println("atualizado.");
+    }
+
+    public void deleteGenero(int ID){
+        Scanner scan = new Scanner(System.in);
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            Genero g1 = em.find(Genero.class, ID);
+
+            if (g1 != null) {
+                System.out.println("Confirmar exclusão? [y/n]");
+                String resposta = scan.nextLine();
+
+                if (resposta.equals("y")) {
+                    em.getTransaction().begin();
+                    em.remove(g1);
+                    em.getTransaction().commit();
+                    System.out.println("Gênero excluída com sucesso");
+                } else if (resposta.equals("n")) {
+                    System.out.println("Operação cancelada");
+                } else {
+                    System.out.println("Resposta inválida");
+                }
+            } else {
+                System.out.println("Gênero não encontrado");
+            }
+        } catch (Exception e) {
+            System.out.println("Ocorreu um erro durante a exclusão: " + e.getMessage());
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+        } finally {
+            em.close();
+            emf.close();
+        }
+    }
+
+  public void addDiretor(int ID, String nome){
+      EntityManager em = emf.createEntityManager();
+      Diretor d1 = new Diretor(ID, nome);
+      em.getTransaction().begin();
+      em.persist(d1);
+      em.getTransaction().commit();
+      em.close();
+  }
+  public Diretor searchDiretor(int ID){
+      EntityManager em = emf.createEntityManager();
+      Diretor diretor = em.find(Diretor.class, ID);
+      em.close();
+      return diretor;
+
+  }
+    public void updateDiretor(int ID){
+        Scanner scan = new Scanner(System.in);
+        Diretor diretor = searchDiretor(ID);
+
+        System.out.println("Digite o novo nome do diretor: ");
+        String novoNome = scan.nextLine();
+        diretor.setNome(novoNome);
+
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.merge(diretor);
+        em.getTransaction().commit();
+        em.close();
+        System.out.println("atualizado.");
+    }
+    public void deleteDiretor(int ID) {
+        Scanner scan = new Scanner(System.in);
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            Diretor d1 = em.find(Diretor.class, ID);
+
+            if (d1 != null) {
+                System.out.println("Confirmar exclusão? [y/n]");
+                String resposta = scan.nextLine();
+
+                if (resposta.equals("y")) {
+                    em.getTransaction().begin();
+                    em.remove(d1);
+                    em.getTransaction().commit();
+                    System.out.println("Diretor excluída com sucesso");
+                } else if (resposta.equals("n")) {
+                    System.out.println("Operação cancelada");
+                } else {
+                    System.out.println("Resposta inválida");
+                }
+            } else {
+                System.out.println("Diretor não encontrada");
+            }
+        } catch (Exception e) {
+            System.out.println("Ocorreu um erro durante a exclusão: " + e.getMessage());
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+        } finally {
+            em.close();
+            emf.close();
+        }
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,128 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int resposta=0;
+        boolean execute = true;
+      do{
+          CRUD crud = new CRUD();
+          System.out.println("----------------MENU----------------");
+          System.out.println("Selecione uma opção: ");
+          System.out.println("1) Cadastrar Mídia ");
+          System.out.println("2) Consultar Mídia ");
+          System.out.println("3) Atualizar Mídia ");
+          System.out.println("4) Deletar Mídia ");
+          System.out.println("5) Cadastrar gênero");
+          System.out.println("6) Consultar gênero");
+          System.out.println("7) Atualizar gênero");
+          System.out.println("8) Deletar gênero");
+          System.out.println("9) Cadastrar diretor");
+          System.out.println("10) Consultar diretor");
+          System.out.println("11) Atualizar diretor");
+          System.out.println("12) Deletar diretor");
+          System.out.println("13) Encerrar programa");
+          System.out.println("----------------*----------------");
+          resposta = scan.nextInt();
+          switch (resposta){
+              case 1:
+                  System.out.println("Identificador da mídia: ");
+                  int id = scan.nextInt();
+                  System.out.println("Nome da mídia: ");
+                  String nome = scan.next();
+                  scan.nextLine();
+                  System.out.println("ID do gênero: ");
+                  int id_genero = scan.nextInt();
+                  Genero g1 = crud.searchGenero(id_genero);
+                  System.out.println("Ano de lançamento: ");
+                  int ano = scan.nextInt();
+                  System.out.println("Id do diretor: ");
+                  int id_diretor = scan.nextInt();
+                  Diretor d1 = crud.searchDiretor(id_diretor);
+                  crud.addMidia(id,nome,g1,ano,d1);
+                  break;
+
+              case 2:
+                  System.out.println("Identificador da mídia: ");
+                  id = scan.nextInt();
+                  Midia m = crud.searchMidia(id);
+                  System.out.println(m);
+                  break;
+
+              case 3:
+                  System.out.println("Identificador da mídia: ");
+                  id = scan.nextInt();
+                  crud.updateMidia(id);
+                  break;
+
+              case 4:
+                  System.out.println("Identificador da mídia: ");
+                  id = scan.nextInt();
+                  crud.deleteMidia(id);
+                  break;
+
+              case 5:
+                  System.out.println("Identificador do gênero: ");
+                  id = scan.nextInt();
+                  System.out.println("Nome do gênero: ");
+                  nome = scan.next();
+                  scan.nextLine();
+                  crud.addGenero(id,nome);
+                  break;
+
+              case 6:
+                  System.out.println("Identificador do gênero: ");
+                  id = scan.nextInt();
+                  Genero g = crud.searchGenero(id);
+                  System.out.println(g.getNome());
+                  break;
+
+              case 7:
+                  System.out.println("Identificador do gênero: ");
+                  id = scan.nextInt();
+                  crud.updateGenero(id);
+                  break;
+
+              case 8:
+                  System.out.println("Identificador do gênero: ");
+                  id = scan.nextInt();
+                  crud.deleteGenero(id);
+                  break;
+
+              case 9:
+                  System.out.println("Identificador do diretor: ");
+                  id = scan.nextInt();
+                  System.out.println("Nome do diretor: ");
+                  nome = scan.next();
+                  scan.nextLine();
+                  crud.addDiretor(id,nome);
+                  break;
+
+              case 10:
+                  System.out.println("Identificador do diretor: ");
+                  id = scan.nextInt();
+                  Diretor d = crud.searchDiretor(id);
+                  System.out.println(d.getNome());
+                  break;
+
+              case 11:
+                  System.out.println("Identificador do diretor: ");
+                  id = scan.nextInt();
+                  crud.updateDiretor(id);
+                  break;
+
+              case 12:
+                  System.out.println("Identificador do diretor: ");
+                  id = scan.nextInt();
+                  crud.deleteDiretor(id);
+                  break;
+
+              case 13:
+                  execute = false;
+                  break;
+          }
+      }
+      while (execute);
+
+    }
+}
